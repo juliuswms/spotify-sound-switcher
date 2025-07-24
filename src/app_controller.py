5122b93613b2f8fa86ea8b3e9f2e16c32196a758
eugene :) <willms.julius@gmx.de>
2025-07-24 13:27:04 +0200

    docs: docstings

--- src/app_controller.py
+++ src/app_controller.py
@@ -1,24 +1,35 @@
+import threading
+import tkinter as tk
+import os
+import sys
+from pathlib import Path
+from subprocess import Popen
+from sys import platform
+
+import pystray
+from PIL import Image
 import config_handler
 import spotify_api
 import hotkey_handler
-import pystray
-import threading
-import tkinter as tk
-import os, sys
-from pathlib import Path
-from subprocess import Popen
 from gui import MainWindow, CredentialsDialog, ErrorDialog
-from PIL import Image
-from sys import platform
 
 VERSION = "1.0.1"
 ICON_PATH = "assets/icon/favicon.ico"
 
 def resource_path(rel):
-        base = Path(getattr(sys, '_MEIPASS', Path(__file__).parent))
-        return base / rel
+    """
+    Transforms a relative path to an absolute path.
+
+    args:
+        rel (str): The relative path to the resource.
+    returns:
+        Path: The absolute path to the resource.
+    """
+    base = Path(getattr(sys, '_MEIPASS', Path(__file__).parent))
+    return base / rel
 
 class AppController:
+    """Main controller for the Spotify Sound Switcher application."""
     def __init__(self):
         self.config_handler = config_handler.ConfigHandler(VERSION)
 
@@ -35,6 +46,13 @@ class AppController:
         self.icon = Image.open(resource_path(ICON_PATH))
 
     def check_credentials(self, force=False):
+        """
+        Check if Spotify credentials are set, prompt for them if not.
+
+        args:
+            force (bool): If True, forces the credentials dialog to show
+            even if credentials are already set.
+        """
         if not self.config_handler.load_config() or force:
             # Temp root window to pass as parent to CredentialsDialog
             temp_root = tk.Tk()
@@ -46,12 +64,18 @@ class AppController:
             temp_root.destroy()
 
     def initialize_spotify(self):
+        """Initialize the Spotify API with the saved credentials."""
         try:
             self.spotify = spotify_api.SpotifyApi(self.config_handler)
         except Exception as e:
             ErrorDialog(f"Error initializing Spotify API: {e}").wait_window()
 
     def set_device_switch_hotkey(self, initial=False):
+        """Set the hotkey for switching devices.
+
+        args:
+            initial (bool): If True, sets the hotkey without removing the existing one.
+        """
         self.hotkey_handler.register_hotkey(self.switch_device, initial)
 
     def switch_device(self):
@@ -69,24 +93,56 @@ class AppController:
                     self.gui_populate_devices(delay_refresh=True)
 
     def is_current_device_available(self):
+        """Check if the current output device is available."""
         current_device = self.spotify.get_current_device()
         if current_device and 'id' in current_device:
             return self.device_is_available(current_device['id'])
         return False
 
     def device_is_available(self, device_id):
+        """
+        Check if a device is available.
+
+        args:
+            device_id (str): The ID of the device to check.
+        returns:
+            bool: True if the device is available, False otherwise.
+        """
         return device_id in [device['id'] for device in self.spotify.get_available_devices()]
 
     def device_has_index(self, device_id):
+        """
+        Check if a device ID is in the selected devices list.
+
+        args:
+            device_id (str): The ID of the device to check.
+        returns:
+            bool: True if the device ID is in the selected devices list, False otherwise.
+        """
         return device_id in self.config_handler.config['selected_devices']
 
     def get_all_devices(self):
+        """
+        Get all available devices, including unavailable ones.
+
+        returns:
+            list: A list of all devices, including unavailable ones.
+        """
         devices = self.spotify.get_available_devices()
-        unavailable_device_ids = [device for device in self.config_handler.config['selected_devices'] if device not in [device['id'] for device in devices]]
-        unavailable_devices = [{"id": device_id, "unavailable": True, "name": device_id, "type": "Unavailable"} for device_id in unavailable_device_ids]
+        unavailable_device_ids = [device for device in self.config_handler.config['selected_devices']
+                                  if device not in [device['id'] for device in devices]]
+        unavailable_devices = [{"id": device_id, "unavailable": True, "name": device_id, "type": "Unavailable"}
+                               for device_id in unavailable_device_ids]
         return devices + unavailable_devices
 
     def minimize_to_tray(self, main_window):
+        """
+        Minimize the main window to the system tray. Hiddes the main window and creates
+        a system tray icon.
+
+        args:
+            main_window (MainWindow): The main window instance to minimize.
+        """
         self.is_tray = True
         self.main_window = main_window
         menu = pystray.Menu(
@@ -97,15 +153,18 @@ class AppController:
         threading.Thread(target=self.tray_icon.run, daemon=True).start()
 
     def restore_from_tray(self):
+        """Restore the main window and stop the tray icon."""
         if self.tray_icon:
             self.tray_icon.stop()
         self.main_window.deiconify()
         self.gui_populate_devices()
 
     def toggle_start_behavior(self):
+        """Toggle the start behavior of the application. (Start in tray or not)"""
         self.config_handler.toggle_start_behavior()
 
     def open_autostart_folder(self):
+        """Open the autostart folder in the file explorer."""
         if platform == "win32":
             startup_folder  = os.path.join(os.getenv("APPDATA"),
                         r"Microsoft\Windows\Start Menu\Programs\Startup")
@@ -114,22 +173,26 @@ class AppController:
             ErrorDialog("Autostart folder opening is not supported on this platform.")
 
     def toggle_close_behavior(self):
+        """Toggle the close behavior of the application. (Close into tray or not)"""
         self.config_handler.toggle_close_behavior()
         self.set_close_behavior(self.main_window)
 
     def set_close_behavior(self, main_window):
+        """Set the close behavior of the main window based on the configuration."""
         if self.config_handler.config.get('close_into_tray', False):
             main_window.set_protocol(main_window.minimize_to_tray)
         else:
             main_window.set_protocol(main_window.destroy)
 
     def destroy_app(self):
+        """Destroy the application, stopping the tray icon if it exists and closing the main window."""
         if self.is_tray:
             self.tray_icon.stop()
 
         self.main_window.after(0, self.main_window.destroy)
 
     def run(self):
+        """Run the main application loop."""
         try:
             self.main_window = MainWindow(self, resource_path(ICON_PATH))
             if self.config_handler.config.get('start_in_tray', False):
